0ca7f92af2975ed288743e05210e9e08a2fbfcd2
sameckmatheus <sameckmatheus>
2023-08-31 09:12:23 -0300

    update: aula002

---
+++ aulas/aula002.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+soma = n1 + n2
+
+message = (f'Os números que vc digitou, são: {n1} e {n2}.\n'
+           f'E a soma entre eles é: {soma}')
+print(message)
