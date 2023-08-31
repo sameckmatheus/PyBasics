270f4162656a01051fd909359eea8a6648af5cda
sameckmatheus <sameckmatheus>
2023-08-31 08:50:39 -0300

    update: texts

--- aulas/aula001.py
+++ aulas/aula001.py
@@ -1,3 +1,6 @@
-message = input('Olá! Digite seu nome aqui: ')
-welcome_message = 'Obrigado pela informação, {}!'.format(message)
-print(welcome_message)
+user_1 = input('Olá! Digite seu nome aqui: ')
+user_2 = input('Olá! Digite mais um: ')
+user_3 = input('Olá! Digite agora o último: ')
+
+message = f'Os nomes que você digitou, são:\n{user_1} \n{user_2} \n{user_3}'
+print(message)
