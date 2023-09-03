a078fdd4e45903569f054ffd867859ab1c854921
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-03 13:58:49 -0300

    update: change calc structure in equation

--- exercicios_externos/tests_unavaibles/adv-ex001.py
+++ exercicios_externos/tests_unavaibles/adv-ex001.py
@@ -8,4 +8,20 @@ c. Se o delta calculado for igual a zero a equação possui apenas uma raiz real
 
 d. Se o delta for positivo, a equação possui duas raízes reais; informe-as ao usuário;'''
 
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valor de c: '))
 
+delta = (b**2 - 4*a*c)
+x1 = (-b + delta**(1/2) / (2*a))
+x2 = (-b - delta**(1/2) / (2*a))
+
+print(f'O valor de X1 é: {x1}')
+print(f'O valor de X2 é: {x2}')
+
+if (a, b, c)!= 0 :
+  print('A equação é completa.')
+  pass
+else:
+  print('A equação é incompleta.')
+  pass
