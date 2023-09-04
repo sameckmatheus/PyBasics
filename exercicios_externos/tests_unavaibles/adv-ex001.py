b91bbcb2e02dcfe99623e49cd45375f3da715de9
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-04 14:41:36 -0300

    update: Finalizando o Exercício

--- exercicios_externos/tests_unavaibles/adv-ex001.py
+++ exercicios_externos/tests_unavaibles/adv-ex001.py
@@ -1,31 +1,30 @@
-# Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c.
-# O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes
-# situações:
+import math
 
-# a. Se o usuário informar o valor de A igual a zero.
-# a equação não é do segundo grau e o programa não deve pedir os demais valores, sendo encerrado;
-# b. Se o delta calculado for negativo, a equação não possui raízes reais. Informe ao usuário e encerre o programa;
-# c. Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe ao usuário;
-# d. Se o delta for positivo, a equação possui duas raízes reais; informe-as ao usuário;
+while True:
+    a = float(input('Entre com o valor de a: '))
+    b = float(input('Entre com o valor de b: '))
+    c = float(input('Entre com o valor de c: '))
 
-a = float(input('Digite o valor de a: '))
-b = float(input('Digite o valor de b: '))
-c = float(input('Digite o valor de c: '))
+    def calcular_raizes():
+        if a == 0:
+            print('A equação não é do segundo grau.')
+            return
 
-delta = (b**2 - 4*a*c)
-x1 = (-b + delta**(1/2) / (2*a))
-x2 = (-b - delta**(1/2) / (2*a))
+        delta = b ** 2 - 4 * a * c
 
-print(f'O valor de X1 é: {x1}')
-print(f'O valor de X2 é: {x2}')
+        if delta < 0:
+            print('A equação não possui raízes reais.')
+        elif delta == 0:
+            x = -b / (2 * a)
+            print(f'A equação possui apenas uma raiz real: {x}')
+        else:
+            x1 = (-b + math.sqrt(delta)) / (2 * a)
+            x2 = (-b - math.sqrt(delta)) / (2 * a)
+            print(f'A equação possui duas raízes reais: {x1} e {x2}')
 
-if (a, b, c) != 0:
-    print('A equação é completa.')
-if a == 0:
-    print('A equação não é do segundo grau.')
-if delta == delta*(-1):
-    print(f'A equação não possui raízes reais: {delta}')
-if delta == 0:
-    print(f'A equação possui apenas uma raiz real: {delta}')
-if delta == delta*(+1):
-    print(f'equação possui duas raízes reais: {delta}')
+
+    calcular_raizes()
+    continuar = input('Deseja sair?\n'
+                      'Digite q ou Enter para novo cálculo: ')
+    if continuar == 'q':
+        break
