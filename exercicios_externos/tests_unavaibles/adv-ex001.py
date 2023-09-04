3b6920bd920d6e0dad17c4e81408480790fbd2eb
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-04 13:59:28 -0300

    update: testando a lógica para o cálculo das raízes da equação

--- exercicios_externos/tests_unavaibles/adv-ex001.py
+++ exercicios_externos/tests_unavaibles/adv-ex001.py
@@ -1,12 +1,12 @@
-''' Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c. O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes situações:  
+# Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c.
+# O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes
+# situações:
 
-a. Se o usuário informar o valor de A igual a zero. a equação não e do segundo grau e o programa não deve fazer pedir os demais valores, sendo encerrado;
-
-b. Se o delta calculado for negativo, a equação não possui raízes reais. Informe ao usuário e encerre o programa;
-
-c. Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe ao usuário;
-
-d. Se o delta for positivo, a equação possui duas raízes reais; informe-as ao usuário;'''
+# a. Se o usuário informar o valor de A igual a zero.
+# a equação não é do segundo grau e o programa não deve pedir os demais valores, sendo encerrado;
+# b. Se o delta calculado for negativo, a equação não possui raízes reais. Informe ao usuário e encerre o programa;
+# c. Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe ao usuário;
+# d. Se o delta for positivo, a equação possui duas raízes reais; informe-as ao usuário;
 
 a = float(input('Digite o valor de a: '))
 b = float(input('Digite o valor de b: '))
@@ -19,9 +19,13 @@ x2 = (-b - delta**(1/2) / (2*a))
 print(f'O valor de X1 é: {x1}')
 print(f'O valor de X2 é: {x2}')
 
-if (a, b, c)!= 0 :
-  print('A equação é completa.')
-  pass
-else:
-  print('A equação é incompleta.')
-  pass
+if (a, b, c) != 0:
+    print('A equação é completa.')
+if a == 0:
+    print('A equação não é do segundo grau.')
+if delta == delta*(-1):
+    print(f'A equação não possui raízes reais: {delta}')
+if delta == 0:
+    print(f'A equação possui apenas uma raiz real: {delta}')
+if delta == delta*(+1):
+    print(f'equação possui duas raízes reais: {delta}')
