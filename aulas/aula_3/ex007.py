24401aa13aae9e6437b976f72f1e5456d64eb161
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-12 16:53:04 -0300

    update: Change New Exercice With (For)

--- aulas/aula_3/ex007.py
+++ aulas/aula_3/ex007.py
@@ -1,5 +1,5 @@
 # Faça um programa que receba um número e usando laços de repetição calcule e mostre a tabuada desse número.
 
 n = int(input('Digite um número: '))
-for a in range(11):
+for a in range(1, 11):
     print(f'{n} x {a} = {n * a}')
---
+++ aulas/aula_3/ex009.py
@@ -0,0 +1,6 @@
+# Faça um programa que verifique e mostre os números entre 1.000 e 2.000 (inclusive) que,
+# quando divididos por 11 produzam resto igual a 2.
+
+for i in range(1000, 2001):
+    if i % 11 == 2:
+        print(i)
