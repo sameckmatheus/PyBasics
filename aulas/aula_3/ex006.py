af6634b0667805c58eeb30f3e9ccb81ad2c590f4
sameckmatheus <sameckmatheus>
2023-09-12 11:25:44 -0300

    update: change new exercice

--- aulas/aula_3/ex006.py
+++ aulas/aula_3/ex006.py
@@ -1,5 +1,15 @@
 soma = 0
 for x in range(10):
     numero = float(input('Digite um número: '))
-    soma = soma + numero
+    soma = soma + numero / 10
 print(soma)
+
+"""
+    soma = 0
+    for x in range(10):
+        numero = float(input('Digite um número: '))
+        soma = soma + numero
+        m = soma / 10
+    print(m)
+
+"""
