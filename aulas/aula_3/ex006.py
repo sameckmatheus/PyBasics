8b8a838491b4e58432bcfcc49e77f1bf80026725
sameckmatheus <sameckmatheus>
2023-09-12 11:31:38 -0300

    update: change variable escope

--- aulas/aula_3/ex006.py
+++ aulas/aula_3/ex006.py
@@ -1,14 +1,7 @@
 soma = 0
 for x in range(10):
     numero = float(input('Digite um número: '))
-    soma = soma + numero / 10
-print(f'{soma}:.0f')
+    soma = soma + numero
 
-"""
-    soma = 0
-    for x in range(10):
-        numero = float(input('Digite um número: '))
-        soma = soma + numero
-        m = soma / 10
-    print(m)
-"""
+m = soma / 10
+print(m)
