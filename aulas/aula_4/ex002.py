7e446709169ddf27628a935978359e0e8171d7e9
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-19 13:37:12 -0300

    Update: Change New Program

---
+++ aulas/aula_4/ex002.py
@@ -0,0 +1,6 @@
+alunos = int(input('Informe a quantidade de alunos na sala: '))
+quantidade = 0
+
+while quantidade <= alunos:
+    print('Informe as notas deste Estudante: ')
+    MediaEstudante = float(input(f'Este estudante, tem média: '))
