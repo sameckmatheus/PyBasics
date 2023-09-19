0f3aa2f7d1614eb4362f2db92a06d4b78f2cbb7e
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-19 13:40:10 -0300

    Update: Change Modification In Program

--- aulas/aula_4/ex002.py
+++ aulas/aula_4/ex002.py
@@ -1,6 +1,13 @@
 alunos = int(input('Informe a quantidade de alunos na sala: '))
 quantidade = 0
 
-while quantidade <= alunos:
-    print('Informe as notas deste Estudante: ')
-    MediaEstudante = float(input(f'Este estudante, tem média: '))
+if alunos > 0:
+    while quantidade <= alunos:
+        print('Informe as notas deste Estudante: ')
+        MediaEstudante = float(input(f'Este estudante, tem média: '))
+else:
+    print('ENTRADA INVÁLIDA!!!')
+    print('A quantidade de alunos deve ser representada por um número inteiro...')
+    quit()
+
+media = MediaEstudante / alunos
