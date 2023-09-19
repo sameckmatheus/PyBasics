d7e5d2488bba768a31f5fb8ecb453fee577cec40
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-19 13:40:55 -0300

    Update: Change Logic Modification In Program

--- aulas/aula_4/ex002.py
+++ aulas/aula_4/ex002.py
@@ -1,13 +1,6 @@
 alunos = int(input('Informe a quantidade de alunos na sala: '))
 quantidade = 0
 
-if alunos > 0:
-    while quantidade <= alunos:
-        print('Informe as notas deste Estudante: ')
-        MediaEstudante = float(input(f'Este estudante, tem média: '))
-else:
-    print('ENTRADA INVÁLIDA!!!')
-    print('A quantidade de alunos deve ser representada por um número inteiro...')
-    quit()
-
-media = MediaEstudante / alunos
+while quantidade <= alunos:
+    print('Informe as notas deste Estudante: ')
+    MediaEstudante = float(input(f'Este estudante, tem média: '))
