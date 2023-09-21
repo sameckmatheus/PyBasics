ae8391f372ea617e64a8dbffd056612733adc9c1
sameckmatheus <sameckmatheus>
2023-09-21 11:37:24 -0300

    Update: Change Resolution For Program

---
+++ aulas/aula_4/desafio004.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+contador = 1
+a = ' '
+
+while contador <= n:
+    a += str(contador)
+    print(a)
+    contador += 1
---
+++ aulas/aula_4/desafio005.py
@@ -0,0 +1,10 @@
+n = int(input('Digite um número: '))
+x = 0
+
+while x <= n:
+    z = 1
+    while z <= x:
+        print(z, end=' ')
+        z += 1
+    print()
+    x += 1
