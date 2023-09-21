f949ecf2324bf787f8850819ad338b9357b65960
sameckmatheus <sameckmatheus>
2023-09-21 09:26:12 -0300

    Update: Change Resolution For Program

--- aulas/aula_4/ex004.py
+++ aulas/aula_4/ex004.py
@@ -8,10 +8,10 @@
 """
 
 n1 = float(input('Informe a primeira nota: '))
-while n1 < 0 and n1 > 10:
+while 0 > n1 > 10:
     n1 = float(input('Informe a primeira nota, com valores válidos entre 0 e 10: '))
 n2 = float(input('Informe a segunda nota: '))
-while n2 < 0 and n2 > 10:
+while 0 > n2 > 10:
     n2 = float(input('Informe a segunda nota, com valores válidos entre 0 e 10: '))
 media = (n1 + n2) / 2
 print(f'A sua média foi calculada.\n'
--- aulas/aula_4/ex005.py
+++ aulas/aula_4/ex005.py
@@ -6,8 +6,17 @@
 
 while True:
 
+    n1 = float(input('Informe a primeira nota: '))
+    while 0 > n1 > 10:
+        n1 = float(input('Informe a primeira nota, com valores válidos entre 0 e 10: '))
+    n2 = float(input('Informe a segunda nota: '))
+    while 0 > n2 > 10:
+        n2 = float(input('Informe a segunda nota, com valores válidos entre 0 e 10: '))
+    media = (n1 + n2) / 2
+    print(f'A sua média foi calculada.\n'
+          f'Resultado: {media}')
 
-
-    continuar = input('Deseja sair, digite [q]?\n Ou, digite [Enter] para novo cálculo: ')
-    if continuar in 'Qq':
+    continuar = input('\nDeseja sair, digite [q]?\n '
+                      'Ou, digite [Enter] para novo cálculo: ')
+    if continuar == 'q':
         break
