bdc46c30ff81e4fbb5e2f0062b643ba0274130c7
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-24 16:51:44 -0300

    Update: Change New Resolution For Program A

--- listas_arrays_vetores/ex002a.py
+++ listas_arrays_vetores/ex002a.py
@@ -4,6 +4,10 @@ import random
 lista_numeros = [random.randint(1, 100) for _ in range(10)]
 for x in range(len(lista_numeros)):
     menor_indice = x
+    elementos = int(input(
+        'Digite um número aleatório: '
+    ))
+    lista_numeros.append(elementos)
     for z in range(x + 1, len(lista_numeros)):
         if lista_numeros[z] < lista_numeros[x]:
             menor_indice = z
