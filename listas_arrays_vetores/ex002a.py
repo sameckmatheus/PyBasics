61b957d991cd6799e96b0aedbd4c4e7a95cebee4
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-24 16:12:14 -0300

    Update: Change Structure For Program A

--- listas_arrays_vetores/ex002a.py
+++ listas_arrays_vetores/ex002a.py
@@ -1,1 +1,10 @@
 # Crie uma lista com 10 números inteiros aleatórios e ordene-os em ordem crescente.
+
+lista_numeros = []
+
+for reps in range(10):
+    elementos = int(input(
+        'Digite um número aleatório: '
+    ))
+    lista_numeros.append(elementos)
+print(lista_numeros)
