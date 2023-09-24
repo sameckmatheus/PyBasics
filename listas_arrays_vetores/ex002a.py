5f3cb22a42d1230b67b58b84ea21594ca8186434
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-24 16:51:18 -0300

    Update: Change Resolution For Program A

--- listas_arrays_vetores/ex002a.py
+++ listas_arrays_vetores/ex002a.py
@@ -1,13 +1,9 @@
 # Crie uma lista com 10 números inteiros aleatórios e ordene-os em ordem crescente.
+import random
 
-lista_numeros = []
-
-for x in range(10):
+lista_numeros = [random.randint(1, 100) for _ in range(10)]
+for x in range(len(lista_numeros)):
     menor_indice = x
-    elementos = int(input(
-        'Digite um número aleatório: '
-    ))
-    lista_numeros.append(elementos)
     for z in range(x + 1, len(lista_numeros)):
         if lista_numeros[z] < lista_numeros[x]:
             menor_indice = z
