ab6508059ff5900b94a6b50dcfd1cbbce08ed61a
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-24 16:29:16 -0300

    Update: Change New Structure For Program A

--- listas_arrays_vetores/ex002a.py
+++ listas_arrays_vetores/ex002a.py
@@ -2,9 +2,15 @@
 
 lista_numeros = []
 
-for reps in range(10):
+for i1 in range(10):
+    menor_indice = i1
     elementos = int(input(
         'Digite um número aleatório: '
     ))
     lista_numeros.append(elementos)
+    for i2 in range(i1 + 1, len(lista_numeros)):
+        if lista_numeros[i2] < lista_numeros[i1]:
+            menor_indice = i2
+    lista_numeros[i1], lista_numeros[menor_indice] = lista_numeros[menor_indice], lista_numeros[i1]
+
 print(lista_numeros)
