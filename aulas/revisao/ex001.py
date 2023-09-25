5ede374ca67868413f5ab3ddc2c01642d9449bf6
sameckmatheus <sameckmatheus>
2023-09-25 08:58:37 -0300

    Update: Change Resolution For Program

--- aulas/revisao/ex001.py
+++ aulas/revisao/ex001.py
@@ -1,15 +1,25 @@
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
+while True:
+    n1 = float(input('\nDigite a primeira nota: '))
+    n2 = float(input('Digite a segunda nota: '))
 
-media = (n1 + n2) / 2
+    media = (n1 + n2) / 2
 
-if media < 7:
-    if media > 4:
-        print(f'Sua média foi ({media}).')
-        print('Você ficou de RECUPERAÇÃO...')
+    if media < 7:
+        if media > 4:
+            print(f'Sua média foi ({media}).')
+            print('Você ficou de RECUPERAÇÃO...')
+        else:
+            print(f'Sua média foi ({media}).')
+            print('Você foi REPROVADO...')
     else:
         print(f'Sua média foi ({media}).')
-        print('Você foi REPROVADO...')
-else:
-    print(f'Sua média foi ({media}).')
-    print('Você foi APROVADO!!!')
+        print('Você foi APROVADO!!!')
+
+    continuar = input(
+        "\nPressione S para realizar um novo cálculo...\n"
+        "Ou, N para encerrar: "
+    )
+
+    if continuar in 'Nn':
+        break
+
