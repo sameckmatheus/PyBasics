50bc839f62c2e76cf7b8b05eda24da50b44574a1
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-28 23:52:09 -0300

    Update: Change Problem 12 Resolution

--- exercicios-prova/ex011.py
+++ exercicios-prova/ex011.py
@@ -5,4 +5,9 @@ e escreva a idade dessa pessoa expressa
 apenas em dias. Considerar ano com 365
 dias e mês com 30 dias.
 """
+idade = int(input('Informe sua idade (anos): '))
+meses = int(input('Informe sua idade (meses): '))
+dias = int(input('Informe sua idade (dias): '))
+soma = idade*365+meses*30+dias
 
+print(f'Você viveu {soma} dias.')
--- exercicios-prova/ex017.py
+++ exercicios-prova/ex017.py
@@ -5,11 +5,10 @@ se forem compradas pelo menos 12. Escreva um
 programa que leia o número de maçãs
 compradas, calcule e escreva o custo total da compra
 """
-
 nMacas = int(input('Informe a quantidade de maçãs que deseja: '))
 if nMacas >= 12:
-  mensagem = f'Valor total da compra: R$ {nMacas*1:.2f}'
-  print(mensagem)
+    mensagem = f'Valor total da compra: R$ {nMacas*1:.2f}'
+    print(mensagem)
 else:
-  mensagem = f'Valor total da compra: R$ {nMacas*1.30:.2f}'
-  print(mensagem)
+    mensagem = f'Valor total da compra: R$ {nMacas*1.30:.2f}'
+    print(mensagem)
