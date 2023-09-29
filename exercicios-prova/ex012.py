25cd45057d3ed53cfc9b6f1f343bc43401e7e872
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-29 01:06:21 -0300

    Update: Change Problem 17 Resolution

--- exercicios-prova/ex012.py
+++ exercicios-prova/ex012.py
@@ -7,3 +7,15 @@ percentual que cada um representa em
 relação ao total de eleitores.
 """
 
+totalEleitores = int(input('Informe a quantidade de eleitores: '))
+votosBrancos = int(input('Informe a quantidade de votos brancos: '))
+votosValidos = int(input('Informe a quantidade de votos válidos: '))
+votosNulos = int(input('Informe a quantidade de votos nulos: '))
+
+percentualVotosBrancos = (votosBrancos / totalEleitores) * 100
+percentualVotosValidos = (votosValidos / totalEleitores) * 100
+percentualVotosNulos = (votosNulos / totalEleitores) * 100
+
+print(f'Votos Brancos: {percentualVotosBrancos}%')
+print(f'Votos Válidos: {percentualVotosValidos}%')
+print(f'Votos Nulos: {percentualVotosNulos}%')
