ba37715bb37b4ffd30a930c6c915169c3ca5ed4c
sameckmatheus <sameckmatheus>
2023-10-11 10:58:33 -0300

    update: change new programs about defs

---
+++ funcoes/exercicio_6/biblioteca.py
@@ -0,0 +1,7 @@
+def numero_inteiro(numero):
+    if numero > 0:
+        return 'P'
+    elif numero < 0:
+        return 'N'
+    else:
+        return 'Z'
---
+++ funcoes/exercicio_6/main.py
@@ -0,0 +1,6 @@
+from biblioteca import *
+
+numero = int(input('Digite um número: '))
+verificacao = numero_inteiro(numero)
+
+print(verificacao)
