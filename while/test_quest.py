a66a33d879afe9225a6db0f5290d328d7425160e
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-13 16:21:25 -0300

    Update: Change New Exercise

---
+++ while/test_quest.py
@@ -0,0 +1,9 @@
+count = 0
+
+while count <= 1000:
+    print(f'A caixa de água está enchendo: {count} L')
+    count += 1
+    if count == 1000:
+        print(f'A caixa de água está cheia: {count} L')
+        break
+    
