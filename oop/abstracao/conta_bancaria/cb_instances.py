602b62b70b8df1197705be819b5fe74185429c6a
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-31 22:53:24 -0300

    fix: add new defs and consts for program

--- oop/abstracao/conta_bancaria/cb_instances.py
+++ oop/abstracao/conta_bancaria/cb_instances.py
@@ -1,15 +1,13 @@
 from conta_bancaria import ContaBancaria
 
-Conta1 = ContaBancaria(7474, "Roberto", "Corrente")
+Conta1 = ContaBancaria(7474, "Roberto", "corrente")
 print(Conta1.ativar_conta())
-print(Conta1.ativar_limite())
 print(Conta1.depositar(200))
 print(Conta1.sacar(20))
 print(Conta1.verificar_saldo())
 
-Conta2 = ContaBancaria(9898, "João", "Corrente")
+Conta2 = ContaBancaria(9898, "João", "poupanca")
 Conta2.status = False
-print(Conta2.ativar_limite())
 print(Conta2.depositar(200))
 print(Conta2.sacar(40))
 print(Conta2.verificar_saldo())
--- oop/abstracao/conta_bancaria/conta_bancaria.py
+++ oop/abstracao/conta_bancaria/conta_bancaria.py
@@ -7,7 +7,6 @@ class ContaBancaria:
         self.saldo = 0
         self.status = False
         self.tipo = tipo
-        self.limite = 0
         self.nome = nome
 
     def depositar(self, valor):
@@ -18,40 +17,24 @@ class ContaBancaria:
                     f'Depósito de R$ {valor:.2f} foi realizado com sucesso para a conta: {self.numero}. \n'
                     f'Horário do depósito: {horario_transacao}\n')
         else:
-            return f'Não foi possível realizar o depósito, pois a conta: {self.numero} parece estar inavtiva...'
+            return f'Não foi possível realizar o depósito, pois a conta: {self.numero} parece estar inativa...'
 
     def sacar(self, valor):
         if self.status:
-            if self.saldo >= valor:
+            if self.verificar_saldo_suficiente(valor):
                 self.saldo -= valor
                 horario_transacao = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
                 return (f'Saque de R$ {valor:.2f} foi realizado com sucesso para a conta: {self.numero}. \n'
                         f'Horário do saque: {horario_transacao}')
-
-            elif valor > self.saldo:
-                saque = (self.saldo + self.limite) - valor
-                horario_transacao = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-                return (f'Saque de R$ {valor:.2f} foi realizado com sucesso para a conta: {self.numero}. \n'
-                        f'Horário do saque: {horario_transacao}')
-
             else:
-                return f'Saldo para saque de R$ {valor:.2f} insuficiente.'
+                raise SaldoInsuficienteError(f'Saldo para saque de R$ {valor:.2f} insuficiente.')
         else:
-            return f'Não foi possível realizar o saque, pois a conta: {self.numero} parece estar inavtiva...'
+            return f'Não foi possível realizar o saque, pois a conta: {self.numero} parece estar inativa...'
 
     def ativar_conta(self):
         if not self.status:
             self.status = True
             return f'Atenção, sua conta está avita!'
-
-    def ativar_limite(self):
-        if self.limite == 0:
-            if self.status:
-                self.limite = 1000
-                return (f'\nParabéns, você foi contemplado!\n'
-                        f'seu novo limite de conta, é: {self.limite}')
-            else:
-                return 'Você ainda não tem um limite de conta...'
 
     def verificar_saldo(self):
         if self.status:
@@ -59,3 +42,34 @@ class ContaBancaria:
                     f'====================================================================================')
         else:
             return f'Não possível verificar o saldo da conta: {self.numero}, pois, a mesma está inativa.'
+
+    def verificar_saldo_suficiente(self, valor):
+        return self.saldo >= valor
+
+
+class LimiteConta:
+    LIMITE_PADRAO_CONTA_CORRENTE = 1000
+    LIMITE_PADRAO_CONTA_POUPANCA = 500
+
+    def __init__(self, tipo_conta):
+        self.tipo_conta = tipo_conta
+        self.limite = self.definir_limite_padrao()
+
+    def definir_limite_padrao(self):
+        if self.tipo_conta == 'corrente':
+            return LimiteConta.LIMITE_PADRAO_CONTA_CORRENTE
+        elif self.tipo_conta == 'poupanca':
+            return LimiteConta.LIMITE_PADRAO_CONTA_POUPANCA
+        else:
+            return 0
+
+
+class Transacao:
+    def __init__(self, valor, tipo_transacao):
+        self.valor = valor
+        self.tipo_transacao = tipo_transacao
+        self.horario_transacao = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+
+class SaldoInsuficienteError(Exception):
+    pass
