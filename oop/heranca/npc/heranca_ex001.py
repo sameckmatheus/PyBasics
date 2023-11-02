bb6f4721bb963b2eb0e3431983d14da85228b013
sameckmatheus <sameckmatheuspro@gmail.com>
2023-11-01 21:35:51 -0300

    chore: add main.py

--- oop/heranca/npc/heranca_ex001.py
+++ oop/heranca/npc/heranca_ex001.py
@@ -39,22 +39,3 @@ class SoldadoElite(NPC):
         self.resistencia = 400
         self.ataque = 860
         super().__init__(nome, grupamento, self.resistencia, self.ataque)
-
-
-npc_1 = Guarda('SnakeEyes',  str('Thunderbolt-9'))
-npc_1.informacoes()
-
-npc_2 = Soldado('IronHands', str('Thunderbolt-9'))
-npc_2.informacoes()
-
-npc_3 = SoldadoElite('Shadow', str('Thunderbolt-9'))
-npc_3.informacoes()
-
-npc_4 = Guarda('LowLight', str('Hellcat'))
-npc_4.informacoes()
-
-npc_5 = Soldado('OnePunch', str('Hellcat'))
-npc_5.informacoes()
-
-npc_6 = SoldadoElite('SilenceMelody', str('Hellcat'))
-npc_6.informacoes()
---
+++ oop/heranca/npc/main.py
@@ -0,0 +1,19 @@
+from heranca_ex001 import *
+
+npc_1 = Guarda('SnakeEyes',  str('Thunderbolt-9'))
+npc_1.informacoes()
+
+npc_2 = Soldado('IronHands', str('Thunderbolt-9'))
+npc_2.informacoes()
+
+npc_3 = SoldadoElite('Shadow', str('Thunderbolt-9'))
+npc_3.informacoes()
+
+npc_4 = Guarda('LowLight', str('Hellcat'))
+npc_4.informacoes()
+
+npc_5 = Soldado('OnePunch', str('Hellcat'))
+npc_5.informacoes()
+
+npc_6 = SoldadoElite('SilenceMelody', str('Hellcat'))
+npc_6.informacoes()
