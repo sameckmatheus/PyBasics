2419a76fa704778311ec8db27d3f66a496c74d37
sameckmatheus <sameckmatheuspro@gmail.com>
2023-11-05 00:33:02 -0300

    fix: add tama's official file

--- desafios/tamagotchi/tamagotchi.py
+++ desafios/tamagotchi/tamagotchi.py
@@ -1,4 +1,47 @@
-from tama import *
+class Tamagotchi:
+    def __init__(self, nome):
+        self.nome = nome
+        self.fome = 50
+        self.felicidade = 50
+        self.energia = 50
 
-meu_tamagotchi = Tamagotchi('Tomate')
+    def alimentar(self):
+        self.fome -= 10
+
+    def brincar(self):
+        self.felicidade += 10
+
+    def dormir(self):
+        self.energia += 20
+
+
+def interagir_com_usuario(tamagotchi):
+    while True:
+        print(f'\nMeu Tamagotchi - {tamagotchi.nome}'
+              f'Fome: {tamagotchi.fome}\n'
+              f'Felicidade: {tamagotchi.felicidade}\n'
+              f'Energia: {tamagotchi.energia}')
+
+        print("O que você quer fazer?")
+        print("1. Alimentar")
+        print("2. Brincar")
+        print("3. Dormir")
+        print("4. Sair")
+
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == "1":
+            tamagotchi.alimentar()
+        elif escolha == "2":
+            tamagotchi.brincar()
+        elif escolha == "3":
+            tamagotchi.dormir()
+        elif escolha == "4":
+            print("Até mais!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+meu_tamagotchi = Tamagotchi("Tama")
 interagir_com_usuario(meu_tamagotchi)
